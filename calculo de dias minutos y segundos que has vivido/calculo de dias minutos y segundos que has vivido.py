479a779647a1e07f9bad53d908b56a7433f4dae3
Laury emmanuel jaquez <99146006+Snake111213@users.noreply.github.com>
2023-02-15 02:28:54 +0100

    Add files via upload

---
+++ calculo de dias minutos y segundos que has vivido/calculo de dias minutos y segundos que has vivido.py
@@ -0,0 +1,16 @@
+
+print('Veamos cuantos dias, minutos y segundos has vivido')
+
+name = input('Nombre: ')
+
+print('Ahora escribe tu edad: ')
+
+age = int(input('Edad: '))
+
+dias = age * 365
+
+minutos = age * 525948
+
+segundos = age * 31556926
+
+print(f'{name} has estado vivo por {dias} dias, {minutos} minutos, {segundos}')
---
+++ calculo de dias minutos y segundos que has vivido/calculo de dias minutos y segundos que has vivido2.py
@@ -0,0 +1,29 @@
+
+
+def calculo_de_tiempo():
+    print('Veamos cuantos dias, minutos y segundos has vivido')
+
+    name = input('Nombre: ')
+
+    print('Ahora escribe tu edad: ')
+
+    age = int(input('Edad: '))
+
+    dias = age * 365
+
+    minutos = age * 525948
+
+    segundos = age * 31556926
+
+    print(f'{name} has estado vivo por {dias} dias, {minutos} minutos, {segundos}')
+
+print('                 bienevenido al calculo del tiempo de tu vida')
+
+calculo_de_tiempo()
+
+x =  input('presione 1 y enter para volver a ejecutarlo de nuevo o 0 para salir:\n ')
+
+if x == "1":
+    calculo_de_tiempo()
+else:
+    print('adios bay')
